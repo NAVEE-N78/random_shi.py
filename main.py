316b7aca3c17d36aef10d19de3ad69b80ccb3565
Naveen Ganesh <119068914+NAVEE-N78@users.noreply.github.com>
2023-07-22 10:10:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,14 @@
-logo = """
- _____________________
-|  _________________  |
-| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
-| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
-|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
-| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
-| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
-| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
-| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
-| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
-| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
-| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
-| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
-|_____________________|
-"""
+import requests#billboardmusikgenerator
+from bs4 import BeautifulSoup
+date=input("which year you want to travel to YYYY-MM-DD:")
+URL =("https://www.billboard.com/charts/hot-100/"+ date)
 
+response = requests.get(URL)
+website_html = response.text
+soup = BeautifulSoup(website_html, 'html.parser')
+song_names = soup.select("li ul li h3")
+song_name=[song.getText().strip() for song in song_names]
+with open("movies.txt", mode="w") as file:
+ for song in song_name:
+  file.write(f"{song}\n")
 
-def add(n1,n2):
-  return n1+n2
-def subtract(n1,n2):
-  return n1-n2
-def multiply(n1,n2):
-  return n1*n2
-def divide(n1,n2):
-  return n1/n2
-
-operations={
-  "+":add,
-   "-" : subtract,
-   "*":multiply,
-   "/":divide,
-}
-
-
-def calculator():
-  print(logo)
-  num1=float(input("Whats the first number?:"))
-  for symbol in operations:
-    print(symbol)
-  should_continue=True
-
-
-
-  while should_continue:
-    operation=input("pick an operation from above:")
-    num2=float(input("Enter your second number:"))
-    calc_operation=operations[operation]
-    answer=calc_operation(num1,num2)
-    print(f"{num1} {operation} {num2} = {answer}")
-
-  
-    if input(f"Type y to confirm calculating with {answer} or n to exit")== "y":
-      num1=answer
-    else:
-      should_continue=False
-      calculator()
-
-calculator()
